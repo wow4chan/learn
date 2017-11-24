0258256af0b333dc44fa0d36e246c38dc0c7eb0b
wowchan <wow4chan@gmail.com>
2017-11-24 20:48:10 +0800

    add lesson 2 about object-oriented-programming-in-java

---
+++ object-oriented-programming-in-java/perimeter/PerimeterRunner.java
@@ -0,0 +1,43 @@
+import edu.duke.*;
+
+public class PerimeterRunner {
+    public double getPerimeter (Shape s) {
+        // Start with totalPerim = 0
+        double totalPerim = 0.0;
+        // Start wth prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim by currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }
+        // totalPerim is the answer
+        return totalPerim;
+    }
+    
+    public int getNumPoints(Shape s) {
+        int totalPoints = 0;
+        for (Point currPt : s.getPoints()) {
+            totalPoints += 1;
+        }
+        return totalPoints;
+    }
+
+    public void testPerimeter () {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        System.out.println("perimeter = " + length);
+        int totalPoints = getNumPoints(s);
+        System.out.println("totalPoints = " + totalPoints);
+    }
+
+    public static void main (String[] args) {
+        PerimeterRunner pr = new PerimeterRunner();
+        pr.testPerimeter();
+    }
+}
